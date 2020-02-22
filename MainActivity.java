f23b8e9d7cbff3d251f37f0ee21878a8d9cf7e49
abhiiishk <41847994+abhiiishk@users.noreply.github.com>
2020-02-23 02:45:11 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.application.webviewapp;
+
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://drive.google.com/open?id=1vjd0pED0nQ3fYQTQK0ikZZ1GOSHIwk4W");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
